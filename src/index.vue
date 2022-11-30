756eaa929f9989f810f833288fdba0475c6c4e35
helox <eyes22798@gmail.com>
2022-11-30 14:00:28 +0800

    refactor:  update test unit spec

--- src/index.vue
+++ src/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <el-button>{{ text }}</el-button>
+  <Button>{{ text }}</Button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api'
+import { Button } from 'element-ui'
 
 export default defineComponent({
   name: 'CustomButton',
+  components: {
+    Button
+  },
   props: {
     text: {
       type: String,
